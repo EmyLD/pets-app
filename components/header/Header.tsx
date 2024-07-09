d1c5acde318642c867a30bb9981b29a85bfc5416
Emy <145982137+EmyLD@users.noreply.github.com>
2024-07-09 12:00:30 +0200

    change header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -9,30 +9,38 @@ const bebas = Bebas_Neue({ weight: "400", subsets: ["latin"] });
 export default function Header() {
   return (
     <>
-      <header>
+      <header className=" flex flex-initial justify-between text-center items-center px-2 py-1">
         <Link href={"/"} className={`${bebas.className} p-4 text-5xl`}>
           nos animaux
         </Link>
         <nav>
-          {userConnected ? (
-            <ul>
-              {" "}
-              <li>coucou</li>
-              <li>coucou</li>
-              <li>coucou</li>
-              <li>coucou</li>
-            </ul>
-          ) : (
-            <ul>
-              {" "}
-              <Navlink href={"/pro"} variant={"outline"}>
-                Vous êtes un éleveur ?
-              </Navlink>
-              <Navlink href={"/login"} variant={"default"}>
-                Se connecter
-              </Navlink>
-            </ul>
-          )}
+          <ul className="flex justify-between">
+            {userConnected ? (
+              <>
+                <Navlink href={"/recherche"} variant={"link"}>
+                  Rechercher
+                </Navlink>
+                <Navlink href={"/information"} variant={"link"}>
+                  M'informer
+                </Navlink>
+                <Navlink href={"/contact"} variant={"link"}>
+                  Contact
+                </Navlink>
+                <Navlink href={"/profil"} variant={"default"}>
+                  Profil
+                </Navlink>
+              </>
+            ) : (
+              <>
+                <Navlink href={"/pro"} variant={"outline"}>
+                  Vous êtes un éleveur ?
+                </Navlink>
+                <Navlink href={"/login"} variant={"default"}>
+                  Se connecter
+                </Navlink>
+              </>
+            )}
+          </ul>
         </nav>
       </header>
     </>
