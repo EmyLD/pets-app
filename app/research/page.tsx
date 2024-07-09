8a97f18d422790226fe647726f1449a4e7727800
Dradoke <nathan.gaudouin76190@gmail.com>
2024-07-09 12:32:57 +0200

    Premier test affichage de page

---
+++ app/research/page.tsx
@@ -0,0 +1,19 @@
+import GeocalisationFiltre from "@/components/geocalisation"
+
+
+
+
+
+
+
+export default async function Page() {
+    
+
+
+    return(
+        <main>
+            <h1>Coucou, tu es sur la page recherche</h1>
+            <GeocalisationFiltre/>
+        </main>
+    )
+}
