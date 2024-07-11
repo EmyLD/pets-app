9d086ac94e2561919aed07588fa5154399b02ac3
JordanFery <j.fery@hotmail.fr>
2024-07-11 21:02:20 +0200

    application de la premiere couche de grid sur formulaire (pas d'image encore)

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -1,153 +1,3 @@
-// "use client"
-// import { zodResolver } from "@hookform/resolvers/zod"
-// import { useForm } from "react-hook-form"
-// import { z } from "zod"
-// import { useState } from "react"
-
-// import { Button } from "@/components/ui/button"
-// import {
-//     Form,
-//     FormControl,
-//     FormDescription,
-//     FormField,
-//     FormItem,
-//     FormLabel,
-//     FormMessage,
-// } from "@/components/ui/form"
-// import { Input } from "@/components/ui/input"
-
-// // Définir le schéma de validation
-// const formSchema = z.object({
-
-//     text: z.string().max(255, {
-//         message: "Votre message est trop long"
-//     }),
-//     firstname: z.string().max(30, {
-//         message: "Veuillez entrer votre prénom"
-//     }),
-//     lastname: z.string().max(30, {
-//         message: "Veuillez entrer votre prénom"
-//     }),
-//     email: z.string().email({
-//         message: "Adresse mail incorrecte"
-//     }),
-//     userType: z.enum(["particulier", "professionnel"], {
-//         required_error: "Veuillez sélectionner une option"
-//     }),
-//     professionnel: z.boolean()
-// });
-
-// export function ContactForm({ onFormSubmit }: { onFormSubmit: () => void }) {
-
-//     const form = useForm<z.infer<typeof formSchema>>({
-//         resolver: zodResolver(formSchema),
-//         defaultValues: {
-//             text: "",
-//             firstname: "",
-//             lastname: "",
-//             email: "",
-//             userType: "particulier", // Default value
-//         },
-//     });
-
-//     // Handler de soumission du formulaire
-//     function onSubmit(values: z.infer<typeof formSchema>) {
-//         console.log(values);
-//         onFormSubmit();
-//     }
-
-//     return (
-//         <main className="flex justify-center pt-6">
-
-//             <Form {...form}>
-//                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-//                     <FormField
-//                         control={form.control}
-//                         name="firstname"
-//                         render={({ field }) => (
-//                             <FormItem>
-//                                 <FormLabel>Prénom</FormLabel>
-//                                 <FormControl>
-//                                     <Input />
-//                                 </FormControl>
-//                                 <FormDescription />
-//                                 <FormMessage />
-//                             </FormItem>
-//                         )}
-//                     />
-//                     <FormField
-//                         control={form.control}
-//                         name="lastname"
-//                         render={({ field }) => (
-//                             <FormItem>
-//                                 <FormLabel>Votre nom</FormLabel>
-//                                 <FormControl>
-//                                     <Input />
-//                                 </FormControl>
-//                                 <FormDescription />
-//                                 <FormMessage />
-//                             </FormItem>
-//                         )}
-//                     />
-//                     <FormField
-//                         control={form.control}
-//                         name="email"
-//                         render={({ field }) => (
-//                             <FormItem>
-//                                 <FormLabel>Adresse mail</FormLabel>
-//                                 <FormControl>
-//                                     <Input />
-//                                 </FormControl>
-//                                 <FormDescription />
-//                                 <FormMessage />
-//                             </FormItem>
-//                         )}
-//                     />
-//                     <FormField
-//                         control={form.control}
-//                         name="userType"
-//                         render={({ field }) => (
-//                             <FormItem>
-//                                 <FormLabel>Particulier</FormLabel>
-//                                 <FormControl>
-//                                     <Input type="radio" />
-//                                 </FormControl>
-//                                 <FormDescription />
-//                                 <FormMessage />
-//                                 <FormLabel>Professionnel</FormLabel>
-//                                 <FormControl>
-//                                     <Input type="radio" />
-//                                 </FormControl>
-//                                 <FormDescription />
-//                                 <FormMessage />
-//                             </FormItem>
-//                         )}
-//                     />
-//                     <FormField
-//                         control={form.control}
-//                         name="text"
-//                         render={({ field }) => (
-//                             <FormItem>
-//                                 <FormLabel>Votre message</FormLabel>
-//                                 <FormControl>
-//                                     <textarea
-//                                         className="w-full h-80 px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500 placeholder-black-500 placeholder-opacity-50"
-//                                         placeholder="Votre message..."
-//                                         {...field}
-//                                     />
-//                                 </FormControl>
-//                                 <FormDescription />
-//                                 <FormMessage />
-//                             </FormItem>
-//                         )}
-//                     />
-//                     <Button type="submit">Submit</Button>
-//                 </form>
-//             </Form>
-
-//         </main>
-//     )
-// }
 
 
 "use client"
@@ -179,7 +29,7 @@ const formSchema = z.object({
         message: "Veuillez entrer votre prénom"
     }),
     lastname: z.string().max(30, {
-        message: "Veuillez entrer votre prénom"
+        message: "Veuillez entrer votre nom"
     }),
     email: z.string().email({
         message: "Adresse mail incorrecte"
@@ -210,108 +60,116 @@ export function ContactForm({ onFormSubmit }: { onFormSubmit: () => void }) {
 
     return (
         <main className="flex justify-center pt-6">
-
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <FormField
-                        control={form.control}
-                        name="firstname"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Prénom</FormLabel>
-                                <FormControl>
-                                    <Input {...field} />
-                                </FormControl>
-                                <FormDescription />
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="lastname"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Votre nom</FormLabel>
-                                <FormControl>
-                                    <Input {...field} />
-                                </FormControl>
-                                <FormDescription />
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Adresse mail</FormLabel>
-                                <FormControl>
-                                    <Input {...field} />
-                                </FormControl>
-                                <FormDescription />
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="userType"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Vous êtes</FormLabel>
-                                <FormControl>
-                                    <div className="flex space-x-4">
-                                        <label className="flex items-center space-x-2">
-                                            <input
-                                                type="radio"
-
-                                                {...field}
-                                                checked={field.value === "particulier"}
-                                                onChange={() => field.onChange("particulier")}
-                                            />
-                                            <span>Particulier</span>
-                                        </label>
-                                        <label className="flex items-center space-x-2">
-                                            <input
-                                                type="radio"
-
-                                                {...field}
-                                                checked={field.value === "professionnel"}
-                                                onChange={() => field.onChange("professionnel")}
-                                            />
-                                            <span>Professionnel</span>
-                                        </label>
-                                    </div>
-                                </FormControl>
-                                <FormDescription />
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="text"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Votre message</FormLabel>
-                                <FormControl>
-                                    <textarea
-                                        className="w-full h-80 px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500 placeholder-black-500 placeholder-opacity-50"
-                                        placeholder="Votre message..."
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormDescription />
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button type="submit">Submit</Button>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-1 gap-6 md:grid-cols-12 md:gap-8">
+                    <div className="md:col-start-3 md:col-span-4">
+                        <FormField
+                            control={form.control}
+                            name="firstname"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Prénom</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormDescription />
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div className="md:col-span-4">
+                        <FormField
+                            control={form.control}
+                            name="lastname"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nom</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormDescription />
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div className="md:col-start-3 md:col-span-8">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Adresse mail</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormDescription />
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div className="md:col-start-3 md:col-span-8">
+                        <FormField
+                            control={form.control}
+                            name="userType"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Vous êtes</FormLabel>
+                                    <FormControl>
+                                        <div className="flex space-x-4">
+                                            <label className="flex items-center space-x-2">
+                                                <input
+                                                    type="radio"
+                                                    {...field}
+                                                    checked={field.value === "particulier"}
+                                                    onChange={() => field.onChange("particulier")}
+                                                />
+                                                <span>Particulier</span>
+                                            </label>
+                                            <label className="flex items-center space-x-2">
+                                                <input
+                                                    type="radio"
+                                                    {...field}
+                                                    checked={field.value === "professionnel"}
+                                                    onChange={() => field.onChange("professionnel")}
+                                                />
+                                                <span>Professionnel</span>
+                                            </label>
+                                        </div>
+                                    </FormControl>
+                                    <FormDescription />
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div className="md:col-start-3 md:col-span-8">
+                        <FormField
+                            control={form.control}
+                            name="text"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Votre message</FormLabel>
+                                    <FormControl>
+                                        <textarea
+                                            className="w-full h-40 md:h-80 px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500 placeholder-black-500 placeholder-opacity-50"
+                                            placeholder="Votre message..."
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription />
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div className="md:col-start-3 md:col-span-8">
+                        <Button type="submit" className="w-full">Submit</Button>
+                    </div>
                 </form>
             </Form>
-
         </main>
     )
 }
