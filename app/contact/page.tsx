699bd97712870aa563dac26c333c3a6fb1418746
JordanFery <j.fery@hotmail.fr>
2024-07-15 12:45:57 +0200

    code clean V2

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,7 @@
 "use client"
 import { useState } from "react";
-import H1 from "@/components/ui/H1";
 import { ContactForm } from "@/components/contactForm";
 import { ContactThanks } from "@/components/contactThanks";
-import Image from "next/image"
 export default function ContactPage() {
     const [formSubmitted, setFormSubmitted] = useState(false);
 
