f8a22c765795a406ab43c8fc9b5517c5f2902524
JordanFery <j.fery@hotmail.fr>
2024-07-15 12:45:27 +0200

    code clean

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,21 +14,7 @@ export default function ContactPage() {
     return (
         <div>
             {!formSubmitted ? (
-                <div className="grid grid-cols-12 gap-4 pt-6">
-                    {/* 
-                        grid-cols-12: Divise la grille en 12 colonnes
-                        gap-4: Ajoute un espace de 4 unités entre les colonnes
-                        */}
-
-                    <div className="col-span-12 md:col-span-6"></div> {/* Colonne vide à gauche pour desktop */}
-                    <div className="col-span-12 md:col-span-6 flex flex-col items-start md:items-center text-left md:text-center"> {/* Colonne avec le texte */}
-                        {/* <H1 className="pt-24">CONTACTEZ-NOUS</H1>
-                        <p className="pt-8">
-                            Une question ? Une suggestion ? Une remarque ? Une demande bien spécifique ? <br />
-                            Contactez-nous, on lira tous vos messages et nous nous engageons à vous répondre rapidement !
-                        </p> */}
-                    </div>
-                </div>
+                ""
             ) : (
                 <ContactThanks />
             )}
